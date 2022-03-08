52a9c7da301d287ff8f6a2aa02f3846e66c728da
Ahmet Bombaci <ahmetbombaci@yahoo.com>
2022-03-07 20:27:00 -0600

    update

---
+++ bin/monitor-media-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://gist.github.com/dideler/85de4d64f66c1966788c1b2304b9caf1
+
+source SECRETS
+
+if [ $( curl -s -o /dev/null -w "%{http_code}" 192.168.0.24:8200) -ne 200 ];
+then
+        curl -X POST \
+             -H 'Content-Type: application/json' \
+                  -d '{"chat_id": "'"$CHAT_ID"'", "text": "Media server is down"}' \
+                       https://api.telegram.org/bot"$BOT_TOKEN"/sendMessage
+fi
---
+++ bin/playtv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+options() {
+  # https://github.com/iptv-org/iptv/blob/master/channels/tr.m3u
+  printf "trt2, https://tv-trt2.medya.trt.com.tr/master_720.m3u8\n"
+  printf "aksutv, https://waw2.artiyerelmedya.net/aksutv/bant1/playlist.m3u8\n"
+  printf "cnn-turk, https://live.duhnet.tv/S2/HLS_LIVE/cnnturknp/track_4_1000/playlist.m3u8\n"
+  printf "next, \n"
+}
+
+echo "Choose tv from dmenu"
+selected=$(options | cut -d, -f1 | dmenu)
+echo "Playing $selected"
+
+station=$(options | grep "^$selected," | cut -d, -f2)
+echo "Playing $station"
+mpv $station
+
