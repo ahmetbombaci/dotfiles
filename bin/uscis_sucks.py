3f99d6e2c5e32bf5b241278406b86f58f79fbcf2
Ahmet Bombaci <ahmetbombaci@yahoo.com>
2022-03-12 11:41:48 -0600

    uscis sucks

---
+++ bin/uscis_sucks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from datetime import date as d; 
+
+print ('#USCISSucks');
+print ('Unemployed, uninsured, no income ', (d.today() - d(2021, 8, 31)).days, 'days.'); 
+print ('No reply to congresswoman ', (d.today() - d(2021, 11, 30)).days, 'days.'); 
+print ('No action to RFE response ', (d.today() - d(2021, 11, 24)).days, 'days.');
